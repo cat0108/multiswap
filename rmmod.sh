f27dfa118c473ddfed035419b6572e5dc2849b7b
cat <1417357696@qq.com>
2024-05-19 14:11:51 +0800

    reset

--- rmmod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo rmmod fastswap.ko
-sudo rmmod fastswap_rdma.ko
--- yokotest/ra_fast/mv.sh
+++
@@ -1,4 +0,0 @@
-vmhgfs-fuse .host:/ /mnt/hgfs
-
-mv trace_record_p.txt /mnt/hgfs/sharefolders
-mv *stat.txt /mnt/hgfs/sharefolders

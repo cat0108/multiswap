0c88d9eab95b8cbe336d42d05f144db1a4615b9f
cat <1417357696@qq.com>
2024-08-12 16:29:37 +0800

    “上传开发文档”

--- dev-rdma/insdram.sh
+++ dev-rdma/insdram.sh
@@ -1,2 +1,3 @@
 sudo insmod multiswap_dram.ko
+sudo insmod remote_schedule.ko
 sudo insmod multiswap.ko
--- dev-rdma/rmdram.sh
+++ dev-rdma/rmdram.sh
@@ -1,2 +1,3 @@
 sudo rmmod multiswap.ko
 sudo rmmod multiswap_dram.ko
+sudo rmmod remote_schedule.ko
